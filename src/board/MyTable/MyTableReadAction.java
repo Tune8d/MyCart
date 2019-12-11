1dd07c20622fc38508d02f607128c85eee06d863
SJR <55867533+Tune8d@users.noreply.github.com>
2019-12-11 14:47:11 +0900

    Update MyTableReadAction.java
    sry

--- src/board/MyTable/MyTableReadAction.java
+++ src/board/MyTable/MyTableReadAction.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import board.db.BoardDAO;
+import board.db.BoardDTO;
 
 public class MyTableReadAction implements Action {
 	
@@ -24,28 +25,29 @@ public class MyTableReadAction implements Action {
 		//BasketAddAction, BasketListAction 참고.
 		// 로그인과 동시에 본 action 이 실행되어야 한다
 		//흐름은.. 로그인->readAction(null이면 신규제작 안내)->create, updated, delete 활성화
-		BoardDAO boardDAO=new BoardDAO();
+		BoardDAO boardDAO= new BoardDAO();
+		BoardDTO boardDTO = new BoardDTO();
 		HttpSession session=request.getSession();
-		String id=(String)session.getAttribute("id");
+		String id= (String) session.getAttribute("userID");
+		ArrayList<BoardDTO> boardList = new ArrayList<BoardDTO>();
+
 		
 		if(id==null){
 			PrintWriter out=response.getWriter();
 			out.println("<script>");
+			out.println("alert('로그인을 해주세요');");
 			out.println("history.go(-1);");
 			out.println("</script>");
 			out.close();
 		}
+
 		
-		Vector vector = boardDAO.read(id);
-		List boardlist=(ArrayList)vector.get(0);
-		List userlist=(ArrayList)vector.get(1);
-		
-		request.setAttribute("boardlist", boardlist);
-		request.setAttribute("userlist", userlist);
+		//boardDTO.setBoardUserID(id);
+		boardList = boardDAO.read(0, id);
 		
 		ActionForward forward=new ActionForward();
 		forward.setRedirect(false);
-		forward.setPath("./board.jsp");
+		forward.setPath("/bring.tb");
 		
 		return forward;
 	}
