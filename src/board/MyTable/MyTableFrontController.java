c021dbb21ebe366a27fd5256aa9e648ab2a9a289
SJR <55867533+Tune8d@users.noreply.github.com>
2019-12-11 20:03:55 +0900

    Update MyTableFrontController.java
    changed method name to actionTB

--- src/board/MyTable/MyTableFrontController.java
+++ src/board/MyTable/MyTableFrontController.java
@@ -1,4 +1,4 @@
-package board.MyTable;
+	package board.MyTable;
 
 import java.io.IOException;
 
@@ -9,44 +9,49 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import user.member.UserMemberLoginAction;
+
 @WebServlet("*.tb")
 public class MyTableFrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public MyTableFrontController() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		actionMT(request, response);
+	public MyTableFrontController() {
+		super();
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		actionTB(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
-	
-	private void actionMT(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			
+
+	private void actionTB(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		
 		String command = null;
 		command = request.getRequestURI().substring(request.getContextPath().length());
-		
 		System.out.println("userCommand:" + command);
-		
+
 		ActionForward forward = null;
 		Action action = null;
-		
-		if(command.equals("/write.tb")) {
+
+		if (command.equals("/write.tb")) {
+			System.out.println("신규회원...");
 			action = new MyTableWriteAction();
-			System.out.println("테이블컨트롤러..");
 			forward = action.execute(request, response);
-		} else if(command.equals("/bring.tb")) {
+		} else if (command.equals("/bring.tb")) {
+			System.out.println("기존회원...");
 			action = new MyTableReadAction();
 			forward = action.execute(request, response);
 		}
-		
-		RequestDispatcher rd = request.getRequestDispatcher(forward.getPath());
-		rd.forward(request, response);
+
+	      if(forward != null) {
+	          RequestDispatcher rd = request.getRequestDispatcher(forward.getPath());
+	          rd.forward(request, response);
+	      }
 	}
 }
