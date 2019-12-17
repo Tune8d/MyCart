45c8a96cc44b55820db2b5cbb04933c2b299715a
SJR <55867533+Tune8d@users.noreply.github.com>
2019-12-17 19:39:23 +0900

    Update MyTableCheckAction.java
    changed the limit of view 10 -> 5

--- src/board/MyTable/MyTableCheckAction.java
+++ src/board/MyTable/MyTableCheckAction.java
@@ -24,7 +24,10 @@ public class MyTableCheckAction implements Action{
 		ActionForward forward = new ActionForward();
 		
 		int page = 1;
-		int limit = 10;
+		int limit = 5;
+		int boardListSum =0;
+		int boardListAvg = 0;
+		int boardListItems = 0;
 		
 		if(request.getParameter("page")!=null){
 			page = Integer.parseInt(request.getParameter("page"));
@@ -32,11 +35,14 @@ public class MyTableCheckAction implements Action{
 		
 		int listCount = boardDAO.getListCount(id);
 		boardList = boardDAO.getBoardList(id, page, limit);
+		boardListSum = boardDAO.getBoardListSum(id);
+		boardListAvg = boardDAO.getBoardListAvg(id);
+		boardListItems = boardDAO.getBoardListItems(id);
 		
 		//총 페이지 수
  		int maxpage=(int)((double)listCount/limit+0.95); //0.95를 더해서 올림 처리
  		//현재 페이지에 보여줄 시작 페이지 수(1, 11, 21 등...)
- 		int startpage = (((int) ((double)page / 10 + 0.9)) - 1) * 10 + 1;
+ 		int startpage = (((int) ((double)page / 10+ 0.9)) - 1) * 10 + 1;
  		//현재 페이지에 보여줄 마지막 페이지 수(10, 20, 30 등...) --> 5로하고싶으면 이 세개 제어
 		int endpage = startpage+10-1;
 		
@@ -49,7 +55,10 @@ public class MyTableCheckAction implements Action{
  		request.setAttribute("endpage", endpage); //현재 페이지에 표시할 끝 페이지 수
 		request.setAttribute("listcount",listCount); //글 수
 		request.setAttribute("boardlist", boardList);
-		
+		request.setAttribute("boardListSum", boardListSum);
+		request.setAttribute("boardListAvg", boardListAvg);
+		request.setAttribute("boardListItems", boardListItems);
+
 		
 		forward.setRedirect(true);
 		forward.setPath("boardCheck.jsp");
