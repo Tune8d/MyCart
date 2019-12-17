86134fff462726a8d57c67459b0250132146d223
SJR <55867533+Tune8d@users.noreply.github.com>
2019-12-18 03:36:25 +0900

    Update MyTableWriteAction.java

--- src/board/MyTable/MyTableWriteAction.java
+++ src/board/MyTable/MyTableWriteAction.java
@@ -22,7 +22,8 @@ public class MyTableWriteAction implements Action {
 		BoardDAO boardDAO = new BoardDAO();
 		BoardDTO boardDTO = new BoardDTO();
 		ActionForward forward = null;
-
+		
+		String boardType = (String) request.getParameter("boardType");
 		String boardTitle = (String) request.getParameter("boardTitle");
 		int boardPrice = Integer.parseInt(request.getParameter("boardPrice"));
 		int boardEa = Integer.parseInt(request.getParameter("boardEa"));
@@ -30,46 +31,39 @@ public class MyTableWriteAction implements Action {
 		String boardSellerLink = request.getParameter("boardSellerLink");
 		String boardTag = request.getParameter("boardTag");
 		String boardUserID = (String) session.getAttribute("userID");
+		
+		boardDTO.setBoardType(boardType);
+		boardDTO.setBoardID(boardDAO.getNext());
+		boardDTO.setBoardTitle(boardTitle);
+		boardDTO.setBoardPrice(boardPrice);
+		boardDTO.setBoardEa(boardEa);
+		boardDTO.setBoardMemo(boardMemo);
+		boardDTO.setBoardSellerLink(boardSellerLink);
+		boardDTO.setBoardTag(boardTag);
+		boardDTO.setBoardDate(boardDAO.getDate());
+		boardDTO.setBoardAvailable(1);
+		boardDTO.setBoardUserID(boardUserID);
 
-		// 주석처리된 값은 DAO 로 자동처리할 값
-		// int boardID = boardDAO.getNext();
-		// System.out.printf("@@@=> %s, %d, %d, %s, %s, %s\n", boardTitle, boardPrice,
-		// boardEa, boardSeller, boardSellerLink, boardUserID);
+		int result = boardDAO.write(boardDTO);
 
+		if (result == 1) {
+			request.setAttribute("data", boardDTO);
 
-			boardDTO.setBoardID(boardDAO.getNext());
-			boardDTO.setBoardTitle(boardTitle);
-			boardDTO.setBoardPrice(boardPrice);
-			boardDTO.setBoardEa(boardEa);
-			boardDTO.setBoardMemo(boardMemo);
-			boardDTO.setBoardSellerLink(boardSellerLink);
-			boardDTO.setBoardTag(boardTag);
-			boardDTO.setBoardDate(boardDAO.getDate());
-			boardDTO.setBoardAvailable(1);
-			boardDTO.setBoardUserID(boardUserID);
-
-			int result = boardDAO.write(boardDTO);
-
-			if (result == 1) {
-				request.setAttribute("data", boardDTO);
-
-				forward = new ActionForward();
-				forward.setPath("boardRead.jsp");
-				return forward;
-			} else if (result == -1) {
-				response.setContentType("text/html; charset=utf-8");
-				PrintWriter out = response.getWriter();
-				out.println("<script>");
-				out.println("alert('추가 실패');");
-				out.println("history.back();");
-				out.println("</script>");
-				out.close();
-			}
-
+			forward = new ActionForward();
+			forward.setRedirect(true);
+			forward.setPath("boardRead.jsp");
 			return forward;
+		} else if (result == -1) {
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>");
+			out.println("alert('추가 실패');");
+			out.println("history.back();");
+			out.println("</script>");
+			out.close();
 		}
 
-
-
+		return forward;
 	}
 
+}
